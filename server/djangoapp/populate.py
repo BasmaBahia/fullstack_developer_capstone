daf7aa9b66d5d1e0f089c8490e10a4bdf8a2cefc
basmabahia <basmahbahia@gmail.com>
2024-10-17 18:59:53 -0400

    cleaned up linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,8 +12,8 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'], 
-            description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'],
+                description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -31,7 +31,7 @@ def initiate():
             "car_make": car_make_instances[1]},
         {"name": "A4", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[2]},
-        {"name": "A5", "type": "SUV", "year": 2023, 
+        {"name": "A5", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[2]},
         {"name": "A6", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[2]},
@@ -51,5 +51,5 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'], 
-            type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
+                                type=data['type'], year=data['year'])
